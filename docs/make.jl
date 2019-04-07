e891303a42dc0af5a39dba7d58bfd31257f07c21
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:04:24 +0000

    Snapshot of branch master taken on 2019-04-07T15:04:23.404-04:00 from "https://github.com/UnofficialJuliaMirror/AxisArrays.jl-39de3d68-74b9-583c-8d2d-e117c070f3a9"

---
+++ docs/make.jl
@@ -0,0 +1,12 @@
+using Documenter, AxisArrays
+
+makedocs(
+    modules = [AxisArrays],
+    sitename = "AxisArrays",
+)
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/JuliaArrays/AxisArrays.jl.git",
+    julia  = "1.0"
+)
